12a2dc0b7ee01b4e673be122eb53f461da0ad44b
Justin Huang <justin.huang.sz@gmail.com>
2023-09-08 06:34:58 +0800

    feat(systemd):fix cannot open host program issue

--- packages/systemd/user-install.sh
+++ packages/systemd/user-install.sh
@@ -4,9 +4,8 @@ cat << EOF
 ===================================
 - Systemd Issue Fix
   Refer to https://github.com/microsoft/WSL/issues/8843
-  This action require one root permission
+  This action require one time root permission request
 ===================================
 EOF
 
-sudo sh -c 'echo :WSLInterop:M::MZ::/init:PF > /usr/lib/binfmt.d/WSLInterop.conf'
-sudo systemctl restart systemd-binfmt
+sudo sh -c 'echo :WSLInterop:M::MZ::/init:PF > /usr/lib/binfmt.d/WSLInterop.conf; sudo systemctl restart systemd-binfmt'
