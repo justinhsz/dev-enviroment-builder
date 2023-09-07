d8c46f617495278e2c5720121ac0796cb5c85473
Justin Huang <justin.huang.sz@gmail.com>
2023-09-08 06:38:36 +0800

    feat(install):add information on install script

--- install.ps1
+++ install.ps1
@@ -25,8 +25,18 @@ if (-not $tarFilePath) {
 
 wsl --import $wslDistroName $wslTargetDir $tarFilePath
 
+Write-Output "First launch the new wsl may take some time..."
 wsl -d $wslDistroName bash -c "/dev-install-files/initial.sh ${env:username}"
 wsl -t $wslDistroName
 
+Write-Output "Follow the instruction to complete your environment setup..."
 wsl -d $wslDistroName bash -c "/dev-install-files/setup-user-environment.sh"
+
+$yn= Read-Host -Prompt "Do you want to set the current wsl distro as default one? (y/n)"
+
+if ( $yn -match "y" ){
+    wsl --set-default $wslDistroName
+}
+
+Write-Output "Setup completed!"
 wsl -d $wslDistroName
