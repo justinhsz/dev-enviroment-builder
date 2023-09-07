1ae8ee75970612b9c9236e764ed2753d9f700dda
Justin Huang <justin.huang.sz@gmail.com>
2023-09-08 06:37:45 +0800

    feat(zsh):install zsh with zimfw and powerlevel10k

--- shells/zsh/user-install.sh
+++ shells/zsh/user-install.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+cat << EOF
+===============================================
+ Zsh Theme/config framework Suggestion
+===============================================
+    - zimfw: zsh config framework with speed
+      https://zimfw.sh
+
+    - powerlevel10k: zsh theme with speed
+      https://github.com/romkatv/powerlevel10k
+===============================================
+EOF
+
+read -p "Do you want to install zim framework and powerlevel10k? (y/n) " yn
+
+if [[ $yn == "y" ]]; then
+  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | sudo zsh
+  echo "zmodule romkatv/powerlevel10k" >> ~/.zimrc
+  zsh -c "zimfw install"
+else
+  echo "Now setup zsh as your default shell"
+  sudo chsh -s $(which zsh) $(whoami)
+fi
