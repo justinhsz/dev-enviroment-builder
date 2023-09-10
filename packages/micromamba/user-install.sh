7179a87c581735be8f9111abc7f4c5f1c966351a
Justin Huang <justin.huang.sz@gmail.com>
2023-09-10 22:00:45 +0800

    refactor(mamba):manage init in rc-file

--- packages/micromamba/user-install.sh
+++ packages/micromamba/user-install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 mkdir -p ~/.conda/envs
 
-echo "Initial mamba to current shell"
-export USER_DEFAULT_SHELL=$(basename "$(getent passwd $(whoami) | awk -F: '{print $NF}')")
-/opt/micromamba/bin/micromamba shell init -s $USER_DEFAULT_SHELL -p ~/.conda/envs
-
-# echo "Make conda as micromamba alias name"
-# echo "alias conda=micromamba" >> ~/.${USER_DEFAULT_SHELL}rc
+# initialization managed in rc-file
